d65bbce0c8c4420debbf56c0a2f19f33d0ec0d36
Sergio Ventura <sergiovforesto@gmail.com>
2024-06-17 10:29:31 -0400

    footer modificado

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -4,7 +4,7 @@ export const Footer = () => {
     return (
         <footer className="mt-auto text-center py-5">
             <div className="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8">
-                <p className="text-sm text-gray-500">`&quot;` El ayacuchano. {new Date().getFullYear()}</p>
+                <p className="text-sm text-gray-500">El ayacuchano. {new Date().getFullYear()}</p>
             </div>
         </footer>
     )
